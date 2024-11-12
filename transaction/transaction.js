0f9021704e0808a0a56d51006979f3394cd28ac8
Gulie3D <julie-causse@live.fr>
2024-11-12 15:49:17 +0100

    first commit

---
+++ transaction/transaction.js
@@ -0,0 +1,7 @@
+document.getElementById("add-transaction-btn").addEventListener("click", function() {
+    document.getElementById("form-container").style.display = "block";
+});
+
+document.getElementById("cancel-btn").addEventListener("click", function() {
+    document.getElementById("form-container").style.display = "none";
+});
